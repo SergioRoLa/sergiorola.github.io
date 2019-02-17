14ea8284b0bef110a304b6d326abef19e2e4d910
Sergio Romero <ing.isc.srl@gmail.com>
2019-02-16 18:58:17 -0600

    First upload

---
+++ precache-manifest.4c0ce94a533c3d8f0172a1939a0239fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e502c2f45960bbdab4d0",
+    "url": "/static/js/main.e502c2f4.chunk.js"
+  },
+  {
+    "revision": "d53b5b030018cbe79d81",
+    "url": "/static/js/2.d53b5b03.chunk.js"
+  },
+  {
+    "revision": "e502c2f45960bbdab4d0",
+    "url": "/static/css/main.74c10ef5.chunk.css"
+  },
+  {
+    "revision": "9555c67727305fc714d0498edcad089e",
+    "url": "/index.html"
+  }
+];
